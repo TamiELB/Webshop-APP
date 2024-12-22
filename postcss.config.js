51de04ec94fc48cf66c7cd9b1a39dbf9d22ce32f
Tami Elb <Tami.dev@outlook.com>
2024-12-22 12:30:46 +0100

    Init project

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+export default {
+    plugins: {
+      tailwindcss: {}, // Add Tailwind CSS processing
+      autoprefixer: {}, // Add Autoprefixer for vendor prefixes
+    },
+  };
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-orange-600 lol">
+    <h1 class="text-cyan-300">ts</h1>
+  </div>
+</template>
+
